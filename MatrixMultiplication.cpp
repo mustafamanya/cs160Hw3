054f4f0018d027f90ca5ab676595846b23cae3d0
mustafamanya <41771186+mustafamanya@users.noreply.github.com>
2021-02-22 10:26:17 -0800

    Add files via upload

---
+++ MatrixMultiplication.cpp
@@ -0,0 +1,124 @@
+
+using namespace std;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <iostream>
+
+
+//change this for bigger matrix
+#define n 4
+
+// maximum size of matrix
+
+int M1[n][n], M2[n][n],M3[n][n];
+int counter=0;
+
+//enter matrix values
+void* createMatrix(int matrix[n][n]){
+
+ for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+         matrix[i][j]=rand()%10;
+        }
+    }
+
+
+return matrix;
+
+}
+
+
+void PrintMatrix(int Matrix[n][n]){
+
+
+   for(int i=0;i<n; i++){
+    for(int j=0; j<n; j++){
+
+        printf("matrix[%d][%d] %d\n",i,j,Matrix[i][j]);
+
+    }
+
+   }
+
+}
+
+
+// each thread will be multiply one row with one col
+void * MatrixMultiply(void *arg){
+
+    int thread=counter++; //each thread will increment counter and therefore multiply a different row*column
+
+for(int i=thread *n/n; i<(thread+1)*n/n; i++){
+
+    for(int j=0; j<n; j++){
+
+        for(int k=0; k<n; k++){
+
+            M3[i][j]=M3[i][j]+ M1[i][k]*M2[k][j];
+        }
+
+    }
+}
+
+
+}
+
+
+
+// had to intialize matrix 3 to 0 or I was getting garbage value
+void *Intialize(int M3[n][n]){
+for(int i=0; i<n; i++){
+
+        for(int j=0; j<n; j++){
+            M3[i][j]=0;
+        }
+    }
+
+return M3;
+
+
+}
+
+int main()
+{
+
+    cout<<"please enter "<<n*n<< " values for matrix 1"<<endl;
+    createMatrix(M1);
+    cout<<"please enter "<<n*n<< " values for matrix 2"<<endl;
+
+
+    createMatrix(M2);
+    cout<<"matrix 1"<<endl;
+    PrintMatrix(M1);
+    cout<<"matrix 2"<<endl;
+    PrintMatrix(M2);
+
+
+    pthread_t threads[n];
+
+    for(int i=0; i<n; i++){
+             int* t;
+        pthread_create(&threads[i], NULL, MatrixMultiply, (void*)(t));
+
+
+    }
+
+     for (int i = 0; i < n; i++){
+        pthread_join(threads[i], NULL);
+     }
+
+     cout<<"multiplication of matrix 1 and 2"<<endl;
+
+     PrintMatrix(M3);
+
+
+
+
+
+
+
+
+    return 0;
+}
